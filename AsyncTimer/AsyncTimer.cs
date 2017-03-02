30a8f6b7d43c99d58e5f3edd36a1e9e7fd684b15
Connor Goddard <hello@connorlukegoddard.com>
2017-03-02 19:58:36 +0000

    Initial commit!

---
+++ AsyncTimer/AsyncTimer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncTimer
+{
+    public class AsyncTimer
+    {
+        public Func<object, EventArgs, Task> OnPollEvent;
+
+        private CancellationTokenSource cancellation = new CancellationTokenSource();
+
+        public AsyncTimer() {
+
+        }
+
+        public void Start()
+        {
+            Task.Run(() => RepeatActionEvery(TimeSpan.FromSeconds(1), cancellation.Token));
+        }
+
+        public void Stop()
+        {
+            cancellation.Cancel();
+        }
+
+        public async Task RepeatActionEvery(TimeSpan interval, CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                
+                try
+                {
+                    Console.WriteLine("Repeat action every.");
+
+                    if (OnPollEvent != null)
+                    {
+                        await OnPollEvent?.Invoke(this, EventArgs.Empty);
+                    }
+                    
+                    //await Task.Run(() => OnPollEvent(this, EventArgs.Empty));
+
+                    Console.WriteLine("Waiting for 1 seconds..");
+                    await Task.Delay(interval, cancellationToken);
+                    Console.WriteLine("Finished waiting, running again.");
+
+                }
+                catch (TaskCanceledException)
+                {
+                    Console.WriteLine("Cancelling the current tracker.");
+                    return;
+                }
+                
+            }
+        }
+    }
+}
---
+++ AsyncTimer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var test = new AsyncTimer();
+            //test.OnPollEvent += Blah;
+            test.Start();
+
+            Console.ReadKey();
+
+            Console.WriteLine("Cancelling...");
+
+            test.Stop();
+
+            Console.ReadKey();
+        }
+
+        public static async Task Blah(object sender, EventArgs args)
+        {
+            Console.WriteLine("Waiting for 5 secs.");
+            await Task.Delay(5000);
+            Console.WriteLine("Completed event handler.");
+        }
+    }
+}
